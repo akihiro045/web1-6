1e0204e7a3d779ab231a1454d269242aac7101dc
Unknown <g1727045@st.t-kougei.ac.jp>
2018-06-04 18:08:09 +0900

    最初のコミット

---
+++ CatEscape/Assets/ArrowController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowController : MonoBehaviour {
+
+    GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        this.player = GameObject.Find("player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //フレームごとに等速で落下
+        transform.Translate(0, -0.1f, 0);
+
+
+        //画面外に出たらオブジェクトを破棄
+        if (transform.position.y < -5.0f)
+        {
+            Destroy(gameObject);
+        }
+
+        //あたり判定
+        Vector2 p1 = transform.position;
+        Vector2 p2 = this.player.transform.position;
+        Vector2 dir = p1 - p2;
+        float d = dir.magnitude;
+        float r1 = 0.5f;
+        float r2 = 1.0f;
+
+        if (d < r1+r2)
+        {
+            //監督スクリプトにプレイヤと衝突したことを伝える
+            GameObject director = GameObject.Find("GameDirector");
+            director.GetComponent<GameDirector>().DecreaseHp();
+            
+            //衝突した場合、矢を消す
+            Destroy(gameObject);
+        }
+    }
+}
